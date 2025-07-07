05635487d6e7002e8c358ecf1164ddf3b0fb6624
ZahaAnass <zahaanass277@gmail.com>
2025-07-07 23:21:26 +0100

    add filters and sorting and searching

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ 
-        name: "vase table",
-    })
+    const products = await Product.find({})
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company } = req.query;
+    const { featured, company, name, sort, fields} = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -16,7 +14,30 @@ const getAllProducts = async (req, res) => {
     if (company) {
         queryObject.company = company;
     }
-    const products = await Product.find(queryObject)
+    if( name) {
+        queryObject.name = { $regex: name, $options: 'i' }; // 'i' for case-insensitive search
+    }
+
+    let result = Product.find(queryObject)
+    let products;
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }else{
+        result = result.sort('createdAt');
+    }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+
+    products = await result;
     res.status(200).json({ products, nbHits: products.length })
 }
 
